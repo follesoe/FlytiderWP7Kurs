030cfbe54ab8c93f65eae37ee233a696aafb470e
Jonas Folleso <jonas@follesoe.no>
2011-03-31 13:37:29 +0200

    Adding missing files.

---
+++ Flytider/DateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Flytider
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+                return ((DateTime) value).ToString(culture);
+            else
+                return ((DateTime)value).ToString(parameter as string, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
